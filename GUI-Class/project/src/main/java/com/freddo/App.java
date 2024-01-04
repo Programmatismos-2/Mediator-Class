b3c67ce5a6efba0afaf09b336946b93720a934a7
heculesk <her.kontos@gmail.com>
2024-01-04 19:43:56 +0200

    Add comments

--- GUI-Class/project/src/main/java/com/freddo/App.java
+++ GUI-Class/project/src/main/java/com/freddo/App.java
@@ -13,7 +13,9 @@ public class App extends Application {
 
 
     public static void main(String[] args) {
+        //Μέθοδος main για εκκίνση προγράμμτος
         launch(args);
+        //Καλεί την launch που εκκινεί την εφαρμογή
     }
 
     @Override
@@ -21,16 +23,20 @@ public class App extends Application {
         stage.setTitle("Chatbot App");
         //Image icon = new Image("chatbot1.jpg");
         //stage.getIcons().add(icon);
+        //Ορισμός τίτλου παραθύρου σε "Chatbot App"
         
 
         try {
             Parent root = FXMLLoader.load(getClass().getResource("MainScene.fxml"));
             Scene scene = new Scene(root);
             stage.setScene(scene);
+            //Δημιουργία νέου Scene με το φορτωμένο layout και ορισμός για το παράθυρο
             stage.show();
+            //Εμφανίζει το παράθυρο
             stage.setOnCloseRequest(event -> {
                 event.consume();
                 logout(stage);
+                //Διαχείριση αιτήματος κλεισίματος παραθύρου καλώντας την "logout"
             });
         } catch(Exception e) {
             e.printStackTrace();
@@ -38,12 +44,16 @@ public class App extends Application {
     }
     public void logout(Stage stage) {
         Alert alert1 = new Alert(AlertType.CONFIRMATION);
+        //Δημιουργία παραθύρου επιβεβαίωσης μέσω της κλάσης alert
         alert1.setTitle("Logout");
+        //Ορισμός τίτλου παραθύρου σε "Logout"
         alert1.setHeaderText("You're about to logout");
         alert1.setContentText("I hope that we could be of help!");
+        //Ενσωμάτωση επικεφαλίδας και περιεχομένου
 
         if (alert1.showAndWait().get() == ButtonType.OK) {
             stage.close();
+            //Αν ο χρήστης πατήσει ΟΚ η μέθοδος κλείνει το stage
         }
     }   
 }
