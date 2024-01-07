a39a9d2df5214749c9ed9ed8bc0782fdc3fd1517
heculesk <her.kontos@gmail.com>
2024-01-07 21:01:02 +0200

    add comments

--- GUI-Class/project/src/main/java/com/freddo/CheckInPage.java
+++ GUI-Class/project/src/main/java/com/freddo/CheckInPage.java
@@ -12,10 +12,15 @@ public class CheckInPage {
     
     public void opencheckWindow() {
         JFrame checkFrame = new JFrame("Check-In");
-        checkFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);   // Κλείστε αυτό το παράθυρο όταν κλείσετε την επιλογή κρατήσεων
+        //Δημιουργία παραθύρου με τίτλο "Check-In"
+        checkFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        // Κλείστε αυτό το παράθυρο όταν κλείσετε την επιλογή κρατήσεων
         checkFrame.setSize(600, 400);
-        checkFrame.setVisible(true);    //Κάνει ορατό το νέο παράθυρο
+        //Ορισμός τοy μeγέθους του παραθύρου
+        checkFrame.setVisible(true);
+        //Κάνει ορατό το νέο παράθυρο
         checkFrame.setLayout(null);
+        //Χρήση null layout -τα στοιχεία τοποθετούνται με συγκεκριμένες συντεταγμένες
         checkFrame.setIconImage(logo.getImage());
 
 
@@ -23,11 +28,13 @@ public class CheckInPage {
         JTextField surnameF1 = new JTextField();
         JTextField flightF1 = new JTextField();
         JTextField emailF1 = new JTextField();
+        //Δημιουργία τεσσάρων πεδίων κειμένου
         
         JLabel nameL1 = new JLabel("Name"); 
         JLabel surnameL1 = new JLabel("Surname");
         JLabel flightL1 = new JLabel("Flight Number");
         JLabel emailL1 = new JLabel("Email");
+        //Δημιουργία τεσσάρων ετικετών 
 
         nameL1.setBounds(50, 50, 100, 30);
         surnameL1.setBounds(50, 110, 100, 30);
@@ -38,6 +45,7 @@ public class CheckInPage {
         surnameF1.setBounds(200, 110, 200, 30);
         flightF1.setBounds(200, 170, 200, 30);
         emailF1.setBounds(200, 230, 200, 30);
+        //Κάθε ετικέτα και πεδίο κειμένου τοποθετούνται σε συγκεκριμένες θέσεις στο παράθυρο
         
         checkFrame.add(nameL1);
         checkFrame.add(surnameL1);
@@ -48,28 +56,39 @@ public class CheckInPage {
         checkFrame.add(surnameF1);
         checkFrame.add(flightF1);
         checkFrame.add(emailF1);
+        //Προσθήκη τεσσάρων ετικετών και πεδίων κειμένου
 
         JButton submitButton = new JButton("Submit");
+        //Προσθήκη κουμπιού JButton με το κείμενο Submit
         submitButton.setBounds(450,300, 100, 30);
+        //Ρύθμιση θέσης και διαστάσεων κουμπιού
         submitButton.setBorder(BorderFactory.createEtchedBorder());
+        //Ρύθμιση περιγράμματος κουμπιού
         checkFrame.add(submitButton);
+        //Προσθήκη κουμπιού στο παράθυρο
 
         submitButton.addActionListener(new ActionListener() {
+            //Ορισμός ActionListener
             @Override
             public void actionPerformed(ActionEvent e) {
+                //Όταν το κουμπί πατηθεί, ο κώδικας που βρίσκεται μέσα στη μέθοδο actionPerformed θα εκτελεστεί
                 if (FlightChecker.checkFlightExistence(surnameF1.getText(), flightF1.getText()) == false) {
                     SaveUsersCredentials.SaveStringToCSV(nameF1.getText(), surnameF1.getText(), flightF1.getText(), emailF1.getText());    //Καλεί την μέθοδο του Mediator για εισαγωγή δεδομένων
+                //Αν η πτήση δεν υπάρχει, τότε καλείται η μέθοδος SaveStringToCSV για να αποθηκευθούν τα δεδομένα του χρήστη σε ένα αρχείο CSV
                 } else {
                     JLabel alr = new JLabel("Είστε ήδη εγκεγραμμένος");
                     alr.setBounds(200, 250, 200, 30);
                     checkFrame.add(alr);
                 }
+                //Αν η πτήση υπάρχει, προστίθεται μήνυμα που ενημερώνει τον χρήστη ότι είναι ήδη εγγεγραμμένος
                 checkFrame.dispose();
+                //Το παράθυρο checkFrame κλείνει
                 SwingUtilities.invokeLater(new Runnable() {
                     @Override
                     public void run() {
                         new Chatbot(surnameF1.getText(), flightF1.getText()).setVisible(true);
                     }
+                    //Δημιουργία ένα νέου παραθύρου Chatbot που εμφανίζει τον διάλογο με τον chatbot
                 });
             }
 
