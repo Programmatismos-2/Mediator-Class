b43ec2d01f3233b66de238c71acc0ced4e29d3a5
heculesk <her.kontos@gmail.com>
2024-01-07 20:54:31 +0200

    add comments

--- GUI-Class/project/src/main/java/com/freddo/Ai.java
+++ GUI-Class/project/src/main/java/com/freddo/Ai.java
@@ -8,8 +8,12 @@ import java.net.http.HttpRequest.BodyPublishers;
  
 public class Ai {
 static HttpClient client = HttpClient.newHttpClient();
+//Δημιουργία μια στατικής μεταβλητής τύπου HttpClient για αποστολή HTTP αιτημάτων
     public static String hey(String question) throws IOException, InterruptedException {
+
+        //Παράγει εξαιρέσεις τύπου IOException και InterruptedException
         String a = GiveFileToAI.fileToVariable(question);
+        //Χρήση κλάσης GiveFileToAI για ανάκτηση περιεχομένου από ένα αρχείο
         String requestBody = "{\n" +
         " \"instances\": [\n" +
         " {\n" +
@@ -25,13 +29,18 @@ static HttpClient client = HttpClient.newHttpClient();
         " \"topK\": 40\n" +
         " }\n" +
         "}";
+        //Δημιουργία αιτήματος
         HttpRequest request = HttpRequest.newBuilder()
+        //Δημιουργία αντικειμένου HttpRequest
         .uri(URI.create("https://server_location-aiplatform.googleapis.com/v1/projects/project_name/locations/server_location/publishers/google/models/text-bison:predict"))
         .POST(BodyPublishers.ofString(requestBody))
         .header("Content-Type", "application/json")
         .header("Authorization", "Bearer " + RunBashCommand.aoth2())
         .build();
+        //Ορίζει διεύθυνση URI, τη μέθοδο (POST), το περιεχόμενο του αιτήματος και κάποια headers
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());;
+        //Αποστολή αιτήματος και λήψη απάντησης σε μορφή συμβολοσειράς
         return ("Response Body: " + response.body());
+        //Επιστροφή απάντησης σε μορφή συμβολοσειράς
     }    
 }
